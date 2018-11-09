a7314a5af4668cd6db0be38bbbdc191bbd844f56
ncwick96 <38059416+ncwick96@users.noreply.github.com>
2018-11-08 22:35:06 -0800

    Update 10minutepandas.py

--- 10minutepandas.py
+++ 10minutepandas.py
@@ -3,7 +3,49 @@
 #
 
 pip install MySQL-connector
-pip install Numpy
+import numpy as np
 pip install Scipy
-pip install Pandas
-pip install matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+s = pd.Series([1,3,5,np.nan,6,8])
+s
+dates = pd.date_range('20130101', periods=6)
+dates
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+df
+df2 = pd.DataFrame({ 'A' : 1.,
+                      'B' : pd.Timestamp('20130102'),
+                      'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
+                      'D' : np.array([3] * 4,dtype='int32'),
+                      'E' : pd.Categorical(["test","train","test","train"]),
+                      'F' : 'foo' })
+df2
+df2.dtypes
+df2.<TAB>
+df.head()
+df.tail(3)
+df.index
+df.columns
+df.values
+df.describe()
+df.T
+df.sort_index(axis=1, ascending=False)
+df.sort_values(by='B')
+df['A']
+df[0:3]
+df['20130102':'20130104']
+df.loc[dates[0]]
+df.loc[:,['A','B']]
+df.loc['20130102':'20130104',['A','B']]
+df.loc['20130102',['A','B']]
+df.loc[dates[0],'A']
+df.at[dates[0],'A']
+df.iloc[3]
+df.iloc[3:5,0:2]
+df.iloc[[1,2,4],[0,2]]
+df.iloc[1:3,:]
+df.iloc[:,1:3]
+df.iloc[1,1]
+df.iat[1,1]
